5d97885c5f30b3d47da23f1c4e0e63a6436b44ea
swayamrajora <127737419+swayamrajora@users.noreply.github.com>
2025-05-13 10:22:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 function stringChop(str, size) {
-
     if (!str) return [];
-
-    // Convert size to a number to avoid type issues
     size = Number(size);
-
-    // Create an array to store chunks
     let chunks = [];
 
-    // Loop through the string in steps of 'size'
+
     for (let i = 0; i < str.length; i += size) {
         chunks.push(str.substring(i, i + size));
     }
@@ -16,7 +11,6 @@ function stringChop(str, size) {
     return chunks;
 }
 
-// Do not change the code below
 const str = prompt("Enter String.");
 const size = prompt("Enter Chunk Size.");
 alert(JSON.stringify(stringChop(str, size)));
